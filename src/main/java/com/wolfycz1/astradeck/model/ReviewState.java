feddb8c74f9fe270b49a62716415a3da9a85dbc3
wolfycz1 <194477237+wolfycz1@users.noreply.github.com>
2026-03-29 18:19:07 +0200

    fix: Switch to Instant in ReviewState

--- src/main/java/com/wolfycz1/astradeck/model/ReviewState.java
+++ src/main/java/com/wolfycz1/astradeck/model/ReviewState.java
@@ -2,14 +2,14 @@ package com.wolfycz1.astradeck.model;
 
 import lombok.Data;
 
-import java.time.LocalDateTime;
+import java.time.Instant;
 import java.util.UUID;
 
 @Data
 public class ReviewState {
     private UUID cardId;
-    private LocalDateTime lastReviewDate;
-    private LocalDateTime nextReviewDate = LocalDateTime.now();
+    private Instant lastReviewDate;
+    private Instant nextReviewDate = Instant.now();
     private int repetitions = 0;
     private int interval = 0;
     private double easeFactor = 2.5;
