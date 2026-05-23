a329b9ba58c8fbc3237dce68a2a33ab4df52801f
wolfycz1 <194477237+wolfycz1@users.noreply.github.com>
2026-05-23 13:04:04 +0200

    refactor: Change database url

--- src/main/java/com/wolfycz1/astradeck/database/DatabaseConfig.java
+++ src/main/java/com/wolfycz1/astradeck/database/DatabaseConfig.java
@@ -2,17 +2,28 @@ package com.wolfycz1.astradeck.database;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.wolfycz1.astradeck.util.OsPaths;
 import org.flywaydb.core.Flyway;
 import org.jdbi.v3.core.Jdbi;
 import org.jdbi.v3.jackson2.Jackson2Config;
 import org.jdbi.v3.jackson2.Jackson2Plugin;
 import org.jdbi.v3.sqlobject.SqlObjectPlugin;
 
+import java.io.IOException;
+import java.nio.file.Files;
+
 public class DatabaseConfig {
-    // ~ change db path later
-    private static final String DB_URL = "jdbc:sqlite:astradeck.db?foreign_keys=on&busy_timeout=5000";
+    private static final String DB_FILENAME = "astradeck.db";
+    private static final String DB_PARAMS = "?foreign_keys=on&busy_timeout=5000";
+    private static final String DB_URL = "jdbc:sqlite:" + OsPaths.DATA_DIR.resolve(DB_FILENAME).toAbsolutePath() + DB_PARAMS;
 
     public static Jdbi initializeDatabase() {
+        try {
+            Files.createDirectories(OsPaths.DATA_DIR);
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to create data directory at " + OsPaths.DATA_DIR, e);
+        }
+
         Flyway flyway = Flyway.configure().dataSource(DB_URL, "", "").load();
         flyway.migrate();
 
